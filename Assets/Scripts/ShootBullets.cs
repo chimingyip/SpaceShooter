3378d2ff8990e05424bfaa4fcda09dc5e06432e6
Chi-Ming Yip <cmyip99@gmail.com>
2023-07-21 00:09:20 +1000

    Importing more sprites

--- Assets/Scripts/ShootBullets.cs
+++ Assets/Scripts/ShootBullets.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootBullets : MonoBehaviour {
-  public Transform centralFirePoint;
+  public Transform centralFirePoint, leftFirePoint, rightFirePoint;
+  public enum FireMode {
+    Single, 
+    Double, 
+    Triple
+  }
+  public FireMode currentFireMode = FireMode.Single;
   public GameObject bulletPrefab;
   public float bulletForce = 20f;
+  public bool isPoweredUp = false;
+  public float powerUpTimer;
   protected BulletCooldownTimer bulletCooldownTimer;
   private bool isFiring = false;
 
@@ -18,9 +26,31 @@ public class ShootBullets : MonoBehaviour {
       if (!bulletCooldownTimer.CooldownComplete) return;
       bulletCooldownTimer.StartCooldown();
 
-      GameObject bullet = Instantiate(bulletPrefab, centralFirePoint.position, centralFirePoint.rotation);
-      Rigidbody2D centralRb = bullet.GetComponent<Rigidbody2D>();
-      centralRb.AddForce(centralFirePoint.up * bulletForce, ForceMode2D.Impulse);
+      if (isPoweredUp) {
+        powerUpTimer -= Time.deltaTime;
+
+        if (powerUpTimer <= 0) {
+          currentFireMode = FireMode.Single;
+          // powerUpTimer = 0;
+          isPoweredUp = false;
+        }
+      }
+
+      switch (currentFireMode) {
+        case FireMode.Single:
+          GameObject bullet = Instantiate(bulletPrefab, centralFirePoint.position, centralFirePoint.rotation);
+          Rigidbody2D centralRb = bullet.GetComponent<Rigidbody2D>();
+          centralRb.AddForce(centralFirePoint.up * bulletForce, ForceMode2D.Impulse);
+          break;
+        case FireMode.Double:
+          GameObject rightBullet = Instantiate(bulletPrefab, rightFirePoint.position, rightFirePoint.rotation);
+					Rigidbody2D rightRb = rightBullet.GetComponent<Rigidbody2D>();
+					rightRb.AddForce(rightFirePoint.up * bulletForce, ForceMode2D.Impulse);
+					GameObject leftBullet = Instantiate(bulletPrefab, leftFirePoint.position, leftFirePoint.rotation);
+					Rigidbody2D leftRb = leftBullet.GetComponent<Rigidbody2D>();
+					leftRb.AddForce(leftFirePoint.up * bulletForce, ForceMode2D.Impulse);
+          break;
+      }
     }
   }
 
