97020c151f0cbce9fce0c8ce3ec3d94beece8d7d
Chi-Ming Yip <cmyip99@gmail.com>
2023-07-29 21:56:24 +1000

    Add player movement boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  [SerializeField] public float moveSpeed = 1f;
-  private Rigidbody2D rb;
+  // private Rigidbody2D rb;
+  [SerializeField] private float moveSpeed = 1f;
+  [SerializeField] private float rightMoveBoundary = 1f;
+  [SerializeField] private float leftMoveBoundary = -1f;
 
   private Vector2 moveInput;
 
   private void Start() {
-    rb = GetComponent<Rigidbody2D>();
+    // rb = GetComponent<Rigidbody2D>();
   }
 
-  void FixedUpdate() {
-    rb.MovePosition(rb.position + moveInput * moveSpeed * Time.deltaTime);
+  private void FixedUpdate() {
+    PlayerMovementBoundaries();
+    transform.Translate(moveInput * moveSpeed * Time.deltaTime);
   }
 
-  void OnMove(InputValue value) {
+  private void OnMove(InputValue value) {
     moveInput = value.Get<Vector2>();
+  }
+
+  private void PlayerMovementBoundaries() {
+    float xMovementClamp = Mathf.Clamp(transform.position.x, leftMoveBoundary, rightMoveBoundary);
+    Vector2 limitPlayerMovement = new Vector2(xMovementClamp, transform.position.y);
+    transform.position = limitPlayerMovement;
   }
 }
