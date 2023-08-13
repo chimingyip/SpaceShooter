c3d97663a0aae843a59ffcc40b496736ed89eee9
Chi-Ming Yip <cmyip99@gmail.com>
2023-08-13 16:58:03 +1000

    remove magic number

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnWaitTime = 2f;
 
     private void Start() {
         StartCoroutine(SpawnEnemy());
     }
 
     IEnumerator SpawnEnemy() {
-        WaitForSeconds wait = new WaitForSeconds(2);
+        WaitForSeconds wait = new WaitForSeconds(spawnWaitTime);
         while (true) {
             Instantiate(enemyPrefab, new Vector3(Random.Range(-4.298f, 4.298f), transform.position.y, 0), transform.rotation);
             yield return wait;
