7bc3874610bef8ad0d79e2385e799c85f6e98d7d
Chi-Ming Yip <cmyip99@gmail.com>
2023-08-13 17:20:34 +1000

    Power up spawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private float spawnWaitTime = 2f;
+    [SerializeField] private GameObject powerUpPrefab;
+    [SerializeField] private float enemySpawnWaitTime = 2f;
+    [SerializeField] private float powerUpSpawnWaitTime = 15f;
+
 
     private void Start() {
         StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnPowerUp());
     }
 
     IEnumerator SpawnEnemy() {
-        WaitForSeconds wait = new WaitForSeconds(spawnWaitTime);
+        WaitForSeconds wait = new WaitForSeconds(enemySpawnWaitTime);
         while (true) {
             Instantiate(enemyPrefab, new Vector3(Random.Range(-4.298f, 4.298f), transform.position.y, 0), transform.rotation);
+            yield return wait;
+        }
+    }
+
+    IEnumerator SpawnPowerUp() {
+        WaitForSeconds wait = new WaitForSeconds(powerUpSpawnWaitTime);
+        while (true) {
+            Instantiate(powerUpPrefab, new Vector3(Random.Range(-4.298f, 4.298f), transform.position.y, 0), transform.rotation);
             yield return wait;
         }
     }
